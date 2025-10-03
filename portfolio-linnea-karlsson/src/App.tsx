c7624155b2aee00fd9fdb7db3217d0cf92a0acda
lxnneak <lienlinneakarlsson@gmail.com>
2025-10-03 02:23:55 +0200

    Add: intro and projects, general css

--- portfolio-linnea-karlsson/src/App.tsx
+++ portfolio-linnea-karlsson/src/App.tsx
@@ -8,27 +8,68 @@ function App() {
 
   return (
     <>
+      <div className="intro">
+        <div>
+          <h3>Hello, I'm</h3>
+          <h1>Linnéa Karlsson</h1>
+          <p>
+            An aspiring fullstack developer dreaming about making the world a
+            better place.
+          </p>
+          <p>
+            Take a look around to see some of the projects that I've worked on!
+          </p>
+        </div>
+        <img src="https://media.licdn.com/dms/image/v2/D4D03AQFM4fjIUPXABQ/profile-displayphoto-shrink_800_800/B4DZcMVNtYGcAg-/0/1748258578120?e=1762387200&v=beta&t=8Ti-i7w7ikFOma27Q1lYaVgiCKsp5L5M-RZCMN04TCg"></img>
+      </div>
+
+      <h1>Featured projects</h1>
+      <div className="projects">
+        <div className="card">
+          <h3>
+            <a
+              href="https://github.com/lxnneak/java-b-projektuppgift"
+              target="_blank"
+            >
+              Burglar game
+            </a>
+          </h3>
+          <p>
+            Java console game where the player has to defend themselves from a
+            burglar inside their home
+          </p>
+        </div>
+
+        <div className="card">
+          <h3>
+            <a
+              href="https://github.com/tHaraldsson/spring-todo-service"
+              target="_blank"
+            >
+              Spring todo service
+            </a>
+          </h3>
+          <p>
+            RESTful API made with Spring Boot that lets the user manage their
+            tasks
+          </p>
+        </div>
+
+        <div className="card">
+          <h3>
+            <a href="https://github.com/davidlkarlsson/Balanza" target="_blank">
+              Balanza
+            </a>
+          </h3>
+          <p>
+            Web app that challenges the user to not spend money during the work
+            week
+          </p>
+        </div>
+      </div>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Technical skills</h1>
       </div>
-      <h1>Vite + React</h1>
-      <p>hello world</p>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
